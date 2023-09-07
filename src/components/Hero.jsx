bf2f08f55f9421862246e2b5ec56dadc8aaaeb6e
Mohamed El Hachimi El Idrissi <mohamed.rez@gmail.com>
2023-09-07 12:36:35 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export function Hero() {
         for small businesses.
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-        Most bookkeeping software is accurate, but hard to use. We make the
+        Most Ayoub but hard to use. We make the
         opposite trade-off, and hope you don’t get audited.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
